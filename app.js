cde7be844d250cf6b63b1cf74a0e0ca2cd18b1cc
linhdang <linhdang100900@gmail.com>
2023-07-12 16:38:42 +0700

    7th update

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-// const express = require('express');
-
-// const app = express();
-// const clientView = require('./render.js');
-
-// app.use('/assets', express.static('assets'));
-// app.listen(3000, () => console.log('Services start :>'));
-
-// app.get('/*', (req, res, next) => {
-//     res.setHeader('Content-Type', 'text/html');
-//     res.send(clientView.html());
-// })
-
-// index.js
-
-
-
-
 const express = require('express');
 
 const app = express();
@@ -27,26 +9,7 @@ app.listen(3000, () => console.log('Services start :>'));
 app.get('/*', (req, res, next) => {
     res.setHeader('Content-Type', 'text/html');
     res.send(clientView.html());
-});
-module.exports = app
+})
 
-// // index.js
-// const express = require('express')
+index.js
 
-// const app = express()
-// const PORT = 4000
-
-// app.listen(PORT, () => {
-//   console.log(`API listening on PORT ${PORT} `)
-// })
-
-// app.get('/', (req, res) => {
-//   res.send(clientView.html())
-// })
-
-// app.get('/about', (req, res) => {
-//   res.send('This is my about route..... ')
-// })
-
-// // Export the Express API
-// module.exports = app
